693dfc70c2504ce10faaf6bfe99af0f816bf647a
habinezadalvan <habinezadalvan@gmail.com>
2020-01-10 10:43:58 +0200

    feature(login): login a user
    - create login functionality
    
    [Delivers #170623854]

--- src/routes/users/usersRoutes.js
+++ src/routes/users/usersRoutes.js
@@ -2,9 +2,15 @@ import { Router } from 'express';
 import validation from '../../middlewares/validationMiddleware';
 import userSchema from '../../helpers/schemas/userSchema';
 import signupCtrl from '../../controllers/users/signup';
+import auth from '../../middlewares/auth';
+import users from '../../controllers/users/users';
+import loginSchema from '../../helpers/schemas/loginSchema';
+import userLogin from '../../controllers/users/login';
 
 const router = Router();
 
 router.post('/register', [validation(userSchema)], signupCtrl.signup);
+router.post('/signin', [validation(loginSchema)], userLogin.login);
+router.get('/user', [auth], users.getSingleUser);
 
 export default router;
